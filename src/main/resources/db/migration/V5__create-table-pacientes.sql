101e2da509b32c52fe8d68580e9c8ceb76d35939
Andres Gomez <andresgomezgarcia95@gmail.com>
2024-07-02 22:53:57 -0400

    first commit

---
+++ src/main/resources/db/migration/V5__create-table-pacientes.sql
@@ -0,0 +1,13 @@
+create table pacientes(
+
+    id bigint not null auto_increment,
+    nombre varchar(100) not null,
+    apellidos varchar(100) not null ,
+    correo varchar(100) not null,
+
+
+
+    primary key(id)
+
+
+);
